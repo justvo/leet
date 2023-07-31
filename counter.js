75e88aa1357ddf654508fc4d1b04252bf2c789b8
justvo <vlad.otyan0.02@gmail.com>
2023-07-31 20:37:05 +0300

    finish

---
+++ counter.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var count = 0;
+var createCounter = function(n) {
+    let count = n;
+    return function() {
+
+        return count++;
+    };
+};
+
+ 
+const counter = createCounter(10)
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
+console.log(counter()) // 13
+ 
