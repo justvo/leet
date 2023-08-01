02f5acc0f088b53ceb1a3675ee4ceefdf6e240fb
justvo <vlad.otyan.02@gmail.com>
2023-08-01 22:27:40 +0300

    add

--- counter2.js
+++ counter2.js
@@ -15,9 +15,3 @@ var createCounter = function(init) {
     
 };
 
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
