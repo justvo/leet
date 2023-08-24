ce3cc1327ee6a29a8fb0ccb713ed19c4fb07ed23
Vlad <129364256+justvo@users.noreply.github.com>
2023-08-24 05:45:52 +0000

    add

---
+++ longestCommonPrefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(!strs.length) return ''
+let longerstCommonPrefix = ''
+for(let i=0; i< strs[0].length; i++) {
+    let char = strs[0][i]
+    if(!char) return ''
+    if(strs.every(str => str[i] === char)) {
+        longerstCommonPrefix+=char
+    } else break
+}
+return longerstCommonPrefix
+
+};
