3418ed84b4987c208a6c5809316868d0a3c015f0
Vlad <129364256+justvo@users.noreply.github.com>
2023-08-24 06:48:06 +0000

    add

---
+++ validParentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if(s.length%2!==0)return false;
+    const stack = [];
+    for(let i =0; i<s.length;i++){
+        let c = s[i];
+        switch(c){
+            case'(': stack.push(')');
+                break;
+            case'{': stack.push('}');
+                break;
+            case'[': stack.push(']');
+                break;
+            default: 
+                if(c!== stack.pop()){
+                    return false;
+                }
+           
+        }
+
+
+        }
+    return stack.length === 0;
+    
+    
+};
