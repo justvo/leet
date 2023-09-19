380469a155eae7223efe8eb1627f163be9c19919
Vlad <129364256+justvo@users.noreply.github.com>
2023-09-19 18:06:11 +0000

    add

---
+++ powerOfFour.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n === 1 || n === 4) return true;
+	if(n < 4) return false;
+	return isPowerOfFour(n/4);
+    
+};
