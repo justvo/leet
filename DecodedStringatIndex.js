2abc5837449c84b6b6b8efde66fa2097c6037682
Vlad <129364256+justvo@users.noreply.github.com>
2023-09-27 06:14:05 +0000

    add

---
+++ DecodedStringatIndex.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var decodeAtIndex = function(s, k) {
+    let len=0;
+    let isDigit=false
+    
+    for(let v of s){
+        if(v>='0'&&v<='9'){
+            len*=+v
+            isDigit=true
+        }else{
+            len++
+            if(len===k&&!isDigit){
+                return s[k-1]
+            }
+        }
+    }
+    
+    for(let i=s.length-1;i>=0;i--){
+        const v=s[i]
+        if(v>='0'&&v<='9'){
+            len=Math.ceil(len/+v) 
+            k%=len
+        }else{
+            if(k===0||k===len){
+                    return v
+               }
+            len--
+        }
+    }
+
+
+    
+};
