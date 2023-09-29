15f5f40044b03c35b1e7d9f4bc300d81bf08717f
Vlad <129364256+justvo@users.noreply.github.com>
2023-09-29 05:31:09 +0000

    add

---
+++ MonotonicArray.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let increasing = true;
+   let decreasing = true;
+
+   for (let i = 1; i < nums.length; i++) {
+       if (nums[i] > nums[i - 1]) {
+           decreasing = false;
+       } else if (nums[i] < nums[i - 1]) {
+           increasing = false;
+       }
+
+       if (!increasing && !decreasing) {
+           return false;
+       }
+   }
+
+   return true; 
+   
+};
