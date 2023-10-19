df0df486e9e1211d32356646a6283909eeee7b6d
Vlad <129364256+justvo@users.noreply.github.com>
2023-10-19 05:41:34 +0000

    add

---
+++ BackspaceStringCompare.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+    let str1 = check(s)
+       let str2 = check(t)
+   
+       return str1 === str2
+   
+       function check(str){
+           let arr = []
+   
+           for(let i =0; i < str.length; i++){
+               if( str[i] == "#") 
+                   arr.pop()
+               else 
+                   arr.push(str[i])
+               
+           }
+   
+           return arr.join("")
+       }
+       
+   };
