94bfb1d141b98206a1641528dbb3d34b95ec2942
justvo <129364256+justvo@users.noreply.github.com>
2023-10-23 05:42:45 +0000

    add

---
+++ ValidMountainArray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    if (arr.length < 3 || arr[0]>=arr[1]) return false;
+    let peak = true;
+    for (let i = 0; i < arr.length - 1; i++) {
+    if (peak && arr[i] > arr[i + 1]) {
+      peak = false;
+    } else if ((!peak && arr[i] <= arr[i + 1])||(arr[i]===arr[i+1])) {
+      return false;
+    }
+  }
+  return peak ? false : true;
+
+};
