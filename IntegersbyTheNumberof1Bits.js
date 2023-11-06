90e8c3d0d579abf3f4dc7cf5f400bbdd0f228d29
justvo <129364256+justvo@users.noreply.github.com>
2023-11-06 11:34:13 +0000

    add

--- IntegersbyTheNumberof1Bits.js
+++ IntegersbyTheNumberof1Bits.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var sortByBits = function(arr) {
-    return arr.sort((a,b)=>{
-        let countA = a.toString(2).split('').filter(bit => bit === '1').length;
-        let countB = b.toString(2).split('').filter(bit => bit === '1').length;
-        return countA - countB || a - b;
-    })
+  return arr.sort((a, b) => {
+    let countA = a.toString(2).split('').filter(bit => bit === '1').length;
+    let countB = b.toString(2).split('').filter(bit => bit === '1').length;
+    return countA - countB || a - b;
+  })
 
 };
