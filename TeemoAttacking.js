aa88ceeccc86b4d12ee937e4465de66d2298379b
Vlad <129364256+justvo@users.noreply.github.com>
2023-12-12 06:43:24 +0000

    2 tasks

---
+++ TeemoAttacking.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function(timeSeries, duration) {
+    let poison = 0;
+       if(!duration||!timeSeries.length) return 0;
+       for(let i=1;i<timeSeries.length;i++) {
+           if(timeSeries[i]-timeSeries[i-1]>duration) {
+               poison+=duration;
+           }
+           else {
+               let exist=timeSeries[i]-timeSeries[i-1];
+               poison+=exist;
+           }        
+       }
+        poison+=duration;
+       return poison;
+   };
