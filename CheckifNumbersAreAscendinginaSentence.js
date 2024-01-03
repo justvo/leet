0a57f354d3c4fc1dc775aceddcd27d72250571ce
Vlad <129364256+justvo@users.noreply.github.com>
2024-01-03 06:26:36 +0000

    add 2 tasks

---
+++ CheckifNumbersAreAscendinginaSentence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areNumbersAscending = function (s) {
+    let numberOfArray = s.match(/\d{1,2}/ig);
+    let output;
+    for (let i = 0; i < numberOfArray.length; i++) {
+        if (numberOfArray[i + 1] - numberOfArray[i] <= 0) {
+            output = false;
+            break;
+        }
+        else
+            output = true;
+    }
+    return output
+}
