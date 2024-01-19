a19711b4444d4149d24652d9306913b8d9de1415
Vlad <129364256+justvo@users.noreply.github.com>
2024-01-19 06:19:47 +0000

    add 2 tasks

---
+++ ShuffletheArray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let res = []
+
+   for(let i =0; i<nums.length/2;i++){
+       res.push(nums[i],nums[i+n])
+   }
+
+   return res;
+};
