26b7d84a58268e93219df8074cdae64ed289b6ed
Vlad <129364256+justvo@users.noreply.github.com>
2024-02-08 09:25:36 +0000

    add 2 tasks

---
+++ range-sum-of-bst.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+    if(!root){
+        return 0
+    }
+
+    let currentVal = (root.val>=low && root.val <=high)?root.val:0;
+
+    let leftSum = rangeSumBST(root.left,low,high)
+    let rightSum = rangeSumBST(root.right,low,high)
+
+    return currentVal+leftSum+rightSum;
+
+};
